008ea65b7028ee63c094ca0c5aa1c3eb7805ace1
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-08-14 19:33:37 -0300

    feat: create database config by beans

---
+++ src/main/java/com/marcos/rodrigues/spring/data/config/DataSourceConfig.java
@@ -0,0 +1,39 @@
+package com.marcos.rodrigues.spring.data.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/spring");
+        dataSource.setUsername("spring");
+        dataSource.setPassword("spring");
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+
+        return dataSource;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.MYSQL);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true);
+        adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+        adapter.setPrepareConnection(true);
+
+        return adapter;
+    }
+
+
+}
