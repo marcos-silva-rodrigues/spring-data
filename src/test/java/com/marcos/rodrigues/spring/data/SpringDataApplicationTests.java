2ee220f514cfb7b3813c26afa03bd0eb56c7c9c9
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-08-14 19:33:01 -0300

    feat: init study

---
+++ src/test/java/com/marcos/rodrigues/spring/data/SpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.rodrigues.spring.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
